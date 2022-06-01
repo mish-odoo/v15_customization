f499832f105a3ddad6d3c8c339de4b7f295eb77f
Mitul Shah <mish@odoo.com>
2022-06-01 10:53:16 +0530

    [REM] remove wrong module

--- mrp_cost/models/mrp_workcenter.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import models, fields
-
-class MrpWorkcenter(models.Model):
-    _inherit = 'mrp.workcenter'
-
-    workcenter_group_id = fields.Many2one('mrp.workcenter.management', string="Work Center Group")
